414efd3bc28c94f2809cb5fd8ab70cb0e2863666
NatalyAlvarado19 <dayanaalvarado606@gmail.com>
2022-02-14 19:26:53 -0600

    ToDo

---
+++ todo.js
@@ -0,0 +1,27 @@
+const txbInputTasks = document.getElementById('txb-input-task');
+const btnAddTask = document.getElementById('btn-add-task');
+const toDoContainer = document.getElementById('to-do-container');
+const containerButton = document.getElementById('btn-add-data');
+
+btnAddTask.addEventListener('click', addTask)
+
+function addTask(){
+    let toDoItem = document.createElement('p')
+    toDoItem.innerHTML = txbInputTasks.value;
+    toDoContainer.appendChild(toDoItem);
+    toDoItem.addEventListener('click', markTask);
+    toDoItem.addEventListener('dblclick', deleteTask);
+    txbInputTasks.value = ' '
+}
+
+function markTask(){
+    if ( this.style.textDecoration == 'line-through'){
+        this.style.textDecoration = 'none'
+    } else {
+    this.style.textDecoration = 'line-through';
+    }
+}
+
+function deleteTask(){
+    toDoContainer.removeChild(this);
+}
